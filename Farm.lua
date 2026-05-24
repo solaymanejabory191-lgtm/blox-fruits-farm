7d9cc812463699f62c3b4ffe99a1389bd359c561
solaymanejabory191-lgtm <solaymanejabory191@gmail.com>
2026-05-24 15:08:05 +0100

    Update Farm.lua
    Ma3art 3lach

--- Farm.lua
+++ Farm.lua
@@ -1,59 +1,62 @@
--- Shari l-UI Library (Rayfield)
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+-- Shari Orion Library (Khfifa ou katkhdem f mobile 100%)
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Window = Rayfield:CreateWindow({
-   Name = "Blox Fruits: Auto Farm",
-   LoadingTitle = "Loading Script...",
-   LoadingSubtitle = "by CustomCoder",
+local Window = OrionLib:MakeWindow({
+    Name = "Blox Fruits: Auto Farm", 
+    HidePremium = false, 
+    SaveConfig = true, 
+    ConfigFolder = "OrionTest"
 })
 
-local Tab = Window:CreateTab("Farm", 4483362458) -- Icon ID
-
--- Variables l-asasiya
+-- Variables
 local _G = getgenv and getgenv() or _G
 _G.AutoFarm = false
 
--- Function bash y-cliki l-script automatic (Auto Clicker)
+-- Function Auto Clicker
 local VirtualUser = game:GetService("VirtualUser")
 game:GetService("Players").LocalPlayer.Idled:Connect(function()
     VirtualUser:Button1Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
 end)
 
--- L-bouton li ghadi y-khdem l-Auto Farm
-local Toggle = Tab:CreateToggle({
-   Name = "Auto Farm All Enemies",
-   CurrentValue = false,
-   Flag = "AutoFarmFlag", 
-   Callback = function(Value)
-       _G.AutoFarm = Value
-       
-       -- L-boucle dyal l-farm
-       spawn(function()
-           while _G.AutoFarm do
-               pcall(function()
-                   local player = game.Players.LocalPlayer
-                   local character = player.Character or player.CharacterAdded:Wait()
-                   local hrp = character:WaitForChild("HumanoidRootPart")
-                   
-                   -- Kay-qlb 3la أقرب monster f l-lo3ba
-                   for _, enemy in pairs(workspace.Enemies:GetChildren()) do
-                       if enemy:FindFirstChild("Humanoid") and enemy.Humanoid.Health > 0 and enemy:FindFirstChild("HumanoidRootPart") then
-                           
-                           -- T-teleporta fawq l-monster b 5 dyal l-steps bash t-farih safely
-                           while _G.AutoFarm and enemy.Humanoid.Health > 0 do
-                               hrp.CFrame = enemy.HumanoidRootPart.CFrame * CFrame.new(0, 5, 0)
-                               
-                               -- Hna fin kat-cliki (khassek tkon haz l-Combat wla l-Sword f ydik)
-                               VirtualUser:CaptureController()
-                               VirtualUser:ClickButton1(Vector2.new(0,0))
-                               
-                               task.wait(0.1) -- Sor3a dyal l-cliks
-                           end
-                       end
-                   end
-               end)
-               task.wait(1)
-           end
-       end)
-   end,
+-- Tab dyal l-farm
+local FarmTab = Window:MakeTab({
+    Name = "Auto Farm",
+    Icon = "rbxassetid://4483362458",
+    PremiumOnly = false
 })
+
+-- Bouton Toggle
+FarmTab:AddToggle({
+    Name = "Auto Farm All Enemies",
+    Default = false,
+    Callback = function(Value)
+        _G.AutoFarm = Value
+        
+        spawn(function()
+            while _G.AutoFarm do
+                pcall(function()
+                    local player = game.Players.LocalPlayer
+                    local character = player.Character or player.CharacterAdded:Wait()
+                    local hrp = character:WaitForChild("HumanoidRootPart")
+                    
+                    for _, enemy in pairs(workspace.Enemies:GetChildren()) do
+                        if enemy:FindFirstChild("Humanoid") and enemy.Humanoid.Health > 0 and enemy:FindFirstChild("HumanoidRootPart") then
+                            while _G.AutoFarm and enemy.Humanoid.Health > 0 and enemy.Parent do
+                                hrp.CFrame = enemy.HumanoidRootPart.CFrame * CFrame.new(0, 5, 0)
+                                
+                                -- Auto Click
+                                VirtualUser:CaptureController()
+                                VirtualUser:ClickButton1(Vector2.new(0,0))
+                                
+                                task.wait(0.1)
+                            end
+                        end
+                    end
+                end)
+                task.wait(1)
+            end
+        end)
+    end    
+})
+
+OrionLib:Init()
